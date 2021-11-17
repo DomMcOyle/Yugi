eb019b4f98527760e1bd6bead3906f1cb334fdbc
Domenico Dell'Olio <zeito99@gmail.com>
2021-11-17 23:19:50 +0100

    aggiunte classe client (traduzione superclasse) e modulo costanti

---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import sys
+import json
+
+import constants
+
+class client():
+    def __init__(self, player, name="Yugi", timeout=60, ipAddress="localhost"):
+        self.serverIp = ipAddress
+        self.timeout = timeout
+        if player.lower() == constants.b_name:
+            self.player = constants.b_player
+            self.port = constants.b_port
+        elif player.lower() == constants.w_name:
+            self.port = constants.w_port
+            self.player = constants.w_player
+        else:
+            raise ValueError("Player role must be BLACK or WHITE")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.serverIp, self.port))
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def write(self, action):
+        #action will be a dictionary
+        jfiglio = json.dumps(action)
+        j_byte = jfiglio.encode('utf-8')
+        self.sock.send(len(j_byte).to_bytes(8,'big'))
+        self.sock.send(j_byte)
+
+    def declare_name(self):
+        name_b = self.name.encode('utf-8')
+        self.sock.send(len(name_b).to_bytes(8,'big'))
+        self.sock.send(name_b)
+        
+    def read(self):
+        len = int(self.sock.recv(8))
+        recieved = self.sock.recv(len)
+        self.current_state = json.loads(recieved.decode(encoding='utf-8'))
---
+++ constants.py
@@ -0,0 +1,11 @@
+### CLIENT ###
+w_name = "white"
+b_name = "black"
+w_port = 5800
+b_port = 5801
+### STATE ###
+w_player = "W"
+b_player ="B"
+w_win = "WW"
+b_win = "BW"
+draw = "D"
